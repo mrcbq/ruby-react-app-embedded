5565684fdcda789c202992d2eb94ee9b1fd5f0b5
mrcbq <marcobaquero@gmail.com>
2023-09-19 17:47:39 -0500

    Add logic to redux reducer

--- app/javascript/redux/greetingsSlice.js
+++ app/javascript/redux/greetingsSlice.js
@@ -1,12 +1,35 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl = '/greetings';
+const baseUrl = '/api/random_greeting';
 const initialState = {
   greetings: [],
   isLoading: true,
   errorMessage: '',
 };
 
-export const fetchRockets = createAsyncThunk('rockets/fetchRockets', () => axios.get(baseUrl).then((response) => response.data));
+export const fetchGreetings = createAsyncThunk('greeting/fetchGreetings', () => axios.get(baseUrl).then((response) => response.data));
 
+const greetingsSlice = createSlice({
+  name: "greeting",
+  initialState,
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreetings.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchGreetings.fulfilled, (state, action) => {
+        const greeting = action.payload;
+        state.isLoading = false;
+        state.greeting = greeting;
+      })
+      .addCase(fetchGreetings.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errorMessage = action.error.message;
+      });
+  },
+
+});
+
+export default greetingsSlice.reducer;
